d5376aacecdef192bc0448abf05eeb6bb6d3b237
Freek <freek@spatie.be>
2018-05-01 20:28:36 +0200

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>CSS for Backend devs</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+<header class="section">
+    <div class="container">
+        <div class="header-top">
+            <h1>XCasts</h1>
+            <a href="#">Sign in</a>
+        </div>
+
+        <nav>
+            <a href="#">Catalog</a>
+            <a href="#">Series</a>
+            <a href="#">Podcast</a>
+            <a href="#">Discussions</a>
+        </nav>
+    </div>
+
+
+</header>
+
+<div class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="box">Some callout text</div>
+            </div>
+            <div class="col">
+                <div class="box">Some callout text</div>
+            </div>
+            <div class="col">
+                <div class="box">Some callout text</div>
+            </div>
+            <div class="col">
+                <div class="box">Some callout text</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
